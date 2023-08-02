983497309c36e392a6868a67e62e739f809f3710
Mateus Sousa <mateussdamiano@gmail.com>
2023-08-02 14:52:00 -0300

    added toast when add product to cart

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { CartProvider } from "@/contexts/CartContext";
 import { usePathname } from "next/navigation";
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -29,6 +30,7 @@ export default function RootLayout({
           <main className={!pathname.startsWith("/studio") ? "px-12 py-6" : ""}>
             {children}
           </main>
+          <Toaster />
         </CartProvider>
       </body>
     </html>
--- src/components/add-to-cart-btn.tsx
+++ src/components/add-to-cart-btn.tsx
@@ -1,15 +1,24 @@
 "use client";
 
-import React from "react";
 import { Button } from "./ui/button";
 import { useCart } from "@/contexts/CartContext";
 import { IProduct } from "@/types/product";
+import { useToast } from "./ui/use-toast";
 
 export default function AddToCartBtn({ product }: { product: IProduct }) {
   const { addProduct } = useCart();
+  const { toast } = useToast();
 
   return (
-    <Button size="lg" onClick={() => addProduct(product)}>
+    <Button
+      size="lg"
+      onClick={() => {
+        addProduct(product);
+        toast({
+          description: "Product added to your shopping cart!",
+        });
+      }}
+    >
       Add to cart
     </Button>
   );
